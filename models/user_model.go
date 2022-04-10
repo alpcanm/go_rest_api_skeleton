056ec949cfd49cafaadfa4ce27e21bb5dfc6b385
Alp Can Marangoz <alpcanm@gmail.com>
2022-04-10 21:49:52 +0300

    removed surname and phoneNumber fields

--- models/user_model.go
+++ models/user_model.go
@@ -5,10 +5,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type UserModel struct {
 	Id                primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
 	Name              string             `json:"name,omitempty" validate:"required"`
-	Surname           string             `json:"surname,omitempty" validate:"required"`
 	Mail              string             `json:"mail,omitempty" validate:"required"`
 	Uid               string             `json:"uid,omitempty" validate:"required"`
-	PhoneNumber       string             `bson:"phone_number" json:"phone_number,omitempty"`
 	MailVerified      bool               `bson:"mail_verified" json:"mail_verified,omitempty"`
 	RaffleNickName    string             `bson:"raffle_nick_name" json:"raffle_nick_name,omitempty"`
 	CreatedAt         int64              `bson:"created_at" json:"created_at,omitempty"`
